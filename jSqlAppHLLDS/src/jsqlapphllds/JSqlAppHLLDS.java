925f211287112f2c16fa562915e0e5371a68efed
hugo lazzari <hugoluiz@outlook.com>
2019-04-22 08:55:09 -0300

    added project files

---
+++ jSqlAppHLLDS/src/jsqlapphllds/JSqlAppHLLDS.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsqlapphllds;
+
+import view.LoginScreen;
+
+/**
+ *
+ * @author hugo lazzari
+ */
+public class JSqlAppHLLDS {
+    public static void main(String[] args) {
+        LoginScreen a = new LoginScreen();
+        a.setVisible(true);
+    }//fechaMain
+    
+}//fechaClasse
